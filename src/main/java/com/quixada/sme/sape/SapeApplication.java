5109ff69dd073bcbdd7280b2780e4a31fb05f915
Robson Cavalcante <Robson Cavalcante>
2016-05-18 11:08:17 -0300

    Commit 1

---
+++ src/main/java/com/quixada/sme/sape/SapeApplication.java
@@ -0,0 +1,12 @@
+package com.quixada.sme.sape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SapeApplication.class, args);
+	}
+}
---
+++ src/main/java/com/quixada/sme/sape/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package com.quixada.sme.sape.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/home")
+	public String index(){
+		return "index";
+	}
+}
