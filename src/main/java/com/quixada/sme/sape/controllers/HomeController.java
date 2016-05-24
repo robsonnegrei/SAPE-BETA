1d64638a875aba54d5942459e3f73c1c106a9ef5
Isaias S. Silva <luiz.isaias95@gmail.com>
2016-05-24 10:37:35 -0300

    Criacao UsuarioDAO e ConnectionFactory
    - UsuarioDAO esta vazio
    - ConnectionFactory esta ok

--- src/main/java/com/quixada/sme/sape/controllers/HomeController.java
+++ src/main/java/com/quixada/sme/sape/controllers/HomeController.java
@@ -3,6 +3,8 @@ package com.quixada.sme.sape.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.quixada.sme.dao.UsuarioDAO;
+
 @Controller
 public class HomeController {
 	@RequestMapping("/home")
