5b5405d5cd006833f9e6305c7d9f5d8193a968c2
romariomartinsdasilva <romariomartinsdasilva@gmail.com>
2016-05-25 11:43:22 -0300

    Criar ProfessorDAO e ImagemDAO
    Criar ProfessorDAO e ImagemDAO
    Criar construtor padrão publico em ProfessorDAO e ImagemDAO

--- src/main/java/com/quixada/sme/dao/UsuarioDAO.java
+++ src/main/java/com/quixada/sme/dao/UsuarioDAO.java
@@ -1,11 +1,5 @@
 package com.quixada.sme.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-import com.quixada.sme.factory.ConnectionFactory;
-
 public class UsuarioDAO {
 	
 }
