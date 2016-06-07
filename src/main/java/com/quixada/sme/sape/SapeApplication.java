4de169ff3272865163757317c2bbc3668b8bec34
RobsonCN2015 <robsoncavalcante.es@gmail.com>
2016-06-07 11:03:06 -0300

    Erro no Antoin Gato
    mudanças : pom.xml, UsuarioDao, ConnectionFactory,
    AutenticacaoController, SapeApplication, login.html

--- src/main/java/com/quixada/sme/sape/SapeApplication.java
+++ src/main/java/com/quixada/sme/sape/SapeApplication.java
@@ -1,12 +1,16 @@
 package com.quixada.sme.sape;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
 
 @SpringBootApplication
 public class SapeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SapeApplication.class, args);
+		SpringApplication.run(SapeApplication.class, args);		
 	}
 }
--- src/main/java/com/quixada/sme/sape/controllers/AutenticacaoController.java
+++ src/main/java/com/quixada/sme/sape/controllers/AutenticacaoController.java
@@ -3,30 +3,43 @@ package com.quixada.sme.sape.controllers;
 import java.sql.SQLException;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.quixada.sme.dao.UsuarioDAO;
 import com.quixada.sme.model.Usuario;
 
 @Controller
 public class AutenticacaoController {
-	@RequestMapping("/autenticar")
+	HttpSession session;
+
+	@RequestMapping(value = "/autenticar", method = RequestMethod.POST)
 	public String autenticar(HttpServletRequest request){
 		UsuarioDAO dao = new UsuarioDAO();
+		if(request.getSession()!= null){
+			session = request.getSession();
+		}
 		try {
 			Usuario usr = dao.buscar(request.getParameter("username"));
 			if (usr == null || usr.getSenha().equals(request.getParameter("password"))) {
-				return "login";
+				session.setAttribute("usuario", usr);
+				if(usr.getIsAdmin()== 1)	
+						return "redirect:../admin/index";
+				else if(usr.getIsProfCoordenadorLei()==1)
+						return "redirect:../PCLei/index";
+				
+				return "redirect:../index";
 			}
 			else{
-				return "/";
+				return "redirect:../login";
 			}
 		} catch (SQLException e) {
 			
 			e.printStackTrace();
-			return "";
+			return "redirect:../login";
 		}
 	}
 }
