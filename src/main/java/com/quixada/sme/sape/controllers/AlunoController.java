85914dcf61c7dbcd009e9cc9c1498dd0cd9b9b44
RobsonCN2015 <robsoncavalcante.es@gmail.com>
2016-06-23 10:45:38 -0300

    Adicionar aluno e excluir aluno finish
    -Falta o Editar Aluno

--- src/main/java/com/quixada/sme/sape/controllers/AlunoController.java
+++ src/main/java/com/quixada/sme/sape/controllers/AlunoController.java
@@ -5,12 +5,15 @@ import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
 
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.quixada.sme.dao.AlunoDAO;
+import com.quixada.sme.dao.EscolaDAO;
 import com.quixada.sme.model.Aluno;
 import com.quixada.sme.model.Usuario;
 
@@ -18,7 +21,7 @@ import com.quixada.sme.model.Usuario;
 public class AlunoController {
 	private static String USUARIO = "usuario";
 
-@RequestMapping(value ={"/PCLei/getAlunos", "getAlunos"})
+@RequestMapping(value ={"PCLei/getAlunos", "getAlunos"})
 	public String listarAlunos(HttpServletRequest request, @RequestParam("idEscola")String idEscola){
 	
 	HttpSession session = request.getSession();
@@ -36,6 +39,7 @@ public class AlunoController {
 	try {
 			int id = Integer.parseInt(idEscola);
 			ArrayList<Aluno> alunos = alunoDAO.buscarAlunosPorEscola(id);
+			session.setAttribute("idEscola", id);
 			
 
 			if(alunos.isEmpty()){
@@ -51,16 +55,49 @@ public class AlunoController {
 			session.setAttribute("erroGetAlunos", true);
 	}
 	
-	return "PCLei/pagAlunos";
+	return "/PCLei/pagAlunos";
 	}
 
-@RequestMapping("/addAluno")
-	public String AddAlunos(){
-		return null;
+@RequestMapping(value={"PCLei/addAluno","/addAluno"})
+	public String AddAlunos(HttpServletRequest request){
+	if(request.getParameter("nome")!= null){
+		int idEscola = Integer.parseInt(request.getParameter("idEscola"));
+		String nome = request.getParameter("nome");
+		int idAluno= 0;
+		AlunoDAO Adao = new AlunoDAO();
+		Aluno aluno = new Aluno(idAluno,idEscola,nome);
+		
+		try {
+			Adao.adiciona(aluno);
+			request.getSession().setAttribute("erroAddAluno", "false");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			request.getSession().setAttribute("erroAddAluno", "true");
+		}
+		
+	}
+	int idEscola = (int) request.getSession().getAttribute("idEscola");
+	return "redirect:/PCLei/getAlunos?idEscola="+idEscola;
 	}
 
-@RequestMapping("/rmAluno")
-	public String excluirAlunos(){
-		return null;
+@RequestMapping(value={"PCLei/rmAluno","/rmAluno"})
+	public String excluirAlunos(HttpServletRequest request){
+		if(request.getParameter("aluno")!= null){
+			int id = Integer.parseInt(request.getParameter("aluno"));
+			//System.err.println(id);
+			AlunoDAO Adao = new AlunoDAO();
+			try {
+				Adao.excluir(id);
+				request.getSession().setAttribute("erroRmAluno", "false");
+	
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				request.getSession().setAttribute("erroRmAluno", "true");
+			}
+		}
+		int idEscola = (int) request.getSession().getAttribute("idEscola");
+		return "redirect:/PCLei/getAlunos?idEscola="+idEscola;
 	}
 }
